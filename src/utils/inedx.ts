a580dd2f64d3c68fc7cd07397d14105ad592d3fd
zhangyongyong <zhangyongyong@newrank.cn>
2022-08-01 15:21:36 +0800

    123

---
+++ src/utils/inedx.ts
@@ -0,0 +1,10 @@
+/**
+ * @description 判断是否是闰年
+ * @param year
+ * @return boolean
+ */
+export const isLeapYear = (year: number | string) => {
+	const date = new Date(+year, 1, 28 + 2)
+	// return (date.getDate() & 1) ? `${+year}年是润年` : `${+year}年8是润年`;
+	return !!(date.getDate() & 1)
+}
