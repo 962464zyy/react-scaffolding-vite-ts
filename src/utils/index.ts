b38729f0035ccd2ab9de19fa84a76130e2b47b22
zhangyongyong <zhangyongyong@newrank.cn>
2022-08-01 16:18:45 +0800

    1

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,6 +6,6 @@
 export const isLeapYear = (year: number | string) => {
 	const date = new Date(+year, 1, 28 + 2)
 	console.log(1)
-	// return (date.getDate() & 1) ? `${+year}年是润年` : `${+year}年8是润年`;
+	// return (date.getDate() & 1) ? `${+year}年是润年` : `${+year}年8是润年`;1
 	return !!(date.getDate() & 1)
 }
